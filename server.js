642c05110f15918bcee936aa90f0064f1722786c
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2022-12-22 02:41:20 +0000

    added the search route to search through the quotes and authors

--- server.js
+++ server.js
@@ -15,22 +15,33 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("<h1>Hi there! my name is Rabie, welcome to my quote server Api</h1>");
+  response.send(
+    "<h1>Hi there! my name is Rabie, welcome to my quote server Api</h1>"
+  );
 });
 
 //START OF YOUR CODE...
 // a route to get all quotes
 app.get("/quotes", function (request, response) {
   response.send(quotes);
-}); 
-
-// route to receive a random quote
-app.get("/quotes/random", function (request, response){
-  const randomQuoteGenerator = quotes[lodash.random(quotes.length - 1)];
-  response.send(randomQuoteGenerator); 
 });
 
+// route to receive a random quote
+app.get("/quotes/random", function (request, response) {
+  const randomQuoteGenerator = quotes[lodash.random(quotes.length - 1)];
+  response.send(randomQuoteGenerator);
+});
 
+// route to search through all quotes
+app.get("/quotes/search", function (request, response) {
+  const term = request.query.term;
+  const filtered = quotes.filter(
+    (quote) =>
+      quote.author.toLowerCase().includes(term.toLowerCase()) ||
+      quote.quote.toLowerCase().includes(term.toLowerCase())
+  );
+  response.send(filtered);
+});
 
 //...END OF YOUR CODE
 
